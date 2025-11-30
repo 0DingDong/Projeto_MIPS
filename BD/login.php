ffd064693e1af98a4abd52f1c53e64c8084c97fb
Roger <rogerioarp.06@gmail.com>
2025-11-30 17:36:03 +0000

    BD online_2

--- BD/login.php
+++ BD/login.php
@@ -7,7 +7,7 @@ $email = $_POST['email_aluno'];
 $password = $_POST['pass_aluno'];
 
 // --------------------- LOGIN ALUNO ---------------------
-$sqlAluno = "SELECT * FROM Aluno WHERE email_aluno = ?";
+$sqlAluno = "SELECT * FROM aluno WHERE aluno_email = ?";
 $stmt = $conn->prepare($sqlAluno);
 $stmt->bind_param("s", $email);
 $stmt->execute();
@@ -16,16 +16,16 @@ $resultAluno = $stmt->get_result();
 if ($resultAluno->num_rows > 0) {
     $user = $resultAluno->fetch_assoc();
     
-    if ($password === $user['pass_aluno']) {
+    if ($password === $user['aluno_pass']) {
         $_SESSION['user_type'] = "aluno";
-        $_SESSION['user_id'] = $user['Alunoid'];
-        header("Location: aluno.html");
+        $_SESSION['user_id'] = $user['aluno_id'];
+        header("Location: ../html/aluno.html");
         exit();
     }
 }
 
 // --------------------- LOGIN PROFESSOR ---------------------
-$sqlProf = "SELECT * FROM Professor WHERE email_prof = ?";
+$sqlProf = "SELECT * FROM professor WHERE professor_email = ?";
 $stmt = $conn->prepare($sqlProf);
 $stmt->bind_param("s", $email);
 $stmt->execute();
@@ -34,16 +34,16 @@ $resultProf = $stmt->get_result();
 if ($resultProf->num_rows > 0) {
     $user = $resultProf->fetch_assoc();
     
-    if ($password === $user['pass_prof']) {
+    if ($password === $user['professor_pass']) {
         $_SESSION['user_type'] = "professor";
-        $_SESSION['user_id'] = $user['Professor_id'];
-        header("Location: professor.html");
+        $_SESSION['user_id'] = $user['professor_id'];
+        header("Location: ../html/professor.html");
         exit();
     }
 }
 
 // --------------------- LOGIN SEGURANÇA ---------------------
-$sqlSeg = "SELECT * FROM Seguranca WHERE email_Seg = ?";
+$sqlSeg = "SELECT * FROM seguranca WHERE seguranca_email = ?";
 $stmt = $conn->prepare($sqlSeg);
 $stmt->bind_param("s", $email);
 $stmt->execute();
@@ -52,10 +52,10 @@ $resultSeg = $stmt->get_result();
 if ($resultSeg->num_rows > 0) {
     $user = $resultSeg->fetch_assoc();
     
-    if ($password === $user['pass_Seg']) {
+    if ($password === $user['seguranca_pass']) {
         $_SESSION['user_type'] = "seguranca";
-        $_SESSION['user_id'] = $user['Seguranca_id'];
-        header("Location: seguranca.html");
+        $_SESSION['user_id'] = $user['seguranca_id'];
+        header("Location: ../html/seguranca.html");
         exit();
     }
 }
@@ -66,4 +66,3 @@ echo "Email ou senha incorretos.";
 $stmt->close();
 $conn->close();
 ?>
-
