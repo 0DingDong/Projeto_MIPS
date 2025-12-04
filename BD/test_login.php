ecb3fec3e264890f4f3f5bb4d1390ee470c45f20
Roger <rogerioarp.06@gmail.com>
2025-12-04 16:39:33 +0000

    Melhorar a reserva de sala (horas)_V5

--- BD/test_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-session_start();
-
-// Simular um login de professor para teste
-$_SESSION['user_type'] = 'professor';
-$_SESSION['user_id'] = 1; // ID do professor Ana Martins
-
-echo json_encode(['success' => true, 'message' => 'Session iniciada para teste. user_type=professor, user_id=1']);
