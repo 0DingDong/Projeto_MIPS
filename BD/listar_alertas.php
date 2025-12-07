38e700e2da7b1415de37b68f33a054f6e0fc49b7
Roger <rogerioarp.06@gmail.com>
2025-12-07 12:47:23 +0000

    criação de listar_alertas

---
+++ BD/listar_alertas.php
@@ -0,0 +1,11 @@
+<?php
+// listar_alertas.php
+// TODO: Implementar endpoint para listar alertas.
+// Atualmente é um ficheiro placeholder criado em 2025-12-07.
+
+header('Content-Type: application/json');
+
+// Resposta temporária mínima para evitar erros 404 enquanto implementa
+echo json_encode(["success" => false, "message" => "Endpoint listar_alertas.php ainda não implementado."]); 
+
+?>
